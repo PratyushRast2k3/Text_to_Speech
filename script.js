4da13003326decb5312cab9fdac37b38c06fc7bb
Pratyush Rastogi <120497807+PratyushRast2k3@users.noreply.github.com>
2024-03-23 15:11:58 +0530

    initial stages

---
+++ script.js
@@ -0,0 +1,9 @@
+const textarea = document.querySelecto("textarea");
+const button = document.querySelector("button");
+
+const textToSpeech = () => {
+const synth = window.speechSynthesis;
+const text = textarea.value; 
+};
+
+button.addEventListener("click",textToSpeech);
